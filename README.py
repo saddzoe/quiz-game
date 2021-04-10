9d27ce99f38f72dd4ed8c8a9dfc788849cddaafd
Mohammed Olaiya <mohammedolaiya@gmail.com>
2021-04-10 00:06:35 -0400

    Update README.py

--- README.py
+++ README.py
@@ -20,11 +20,9 @@ def verify_guess(guess, answer):
 
 score = 0
 print("Guess the Movie quote: ")\n
-guess1 = input("I was born in the darkness, you were merely molded by it: ")
+guess1 = input("Ah you think darkness is your ally? You merely adopted the dark. I was born in it, molded by it. I didn't see the light until I was already a man, by then it was nothing to me but blinding!: ")
 verify_guess(guess1, "The Dark Knight Rises")
 guess2 = input("How do you get fired on your day off: ")
 verify_guess(guess2, "Friday")
 guess3 = input("You need people like me so you can point your fingers and say, 'That's the bad guy.: ")
 verify_guess(guess3, "Scarface")
-
-print("Your Score is " + str(score))
